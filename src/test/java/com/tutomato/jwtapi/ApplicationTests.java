eb18bba6db9721a202f394e2fceb092223cd8d2f
jongwan <jw.kim@changbi.com>
2023-04-04 09:51:32 +0900

    first init

---
+++ src/test/java/com/tutomato/jwtapi/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutomato.jwtapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
